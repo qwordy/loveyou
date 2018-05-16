a92ee56e53f61d985fa807ae2e8058dd3ec17067
yufeiyue <yufeiyue@baidu.com>
2018-05-16 22:00:42 +0800

    fy

---
+++ SqlUtil.js
@@ -0,0 +1,25 @@
+var sqlite3 = require('sqlite3').verbose();
+
+class SqlUtil {
+  constructor() {
+    this.db = new sqlite3.Database('dat.db');
+  }
+
+  insert(time, alias, event, feeling, done) {
+    db.serialize(function() {
+      var stmt = db.prepare('insert into calendar(time, alias, event, feeling, done) values(?, ?, ?, ?, ?)');
+      stmt.run(time, alias, event, feeling, done);
+      stmt.finalize();
+    });
+  }
+
+  select() {
+
+  }
+
+  close() {
+    this.db.close()
+  }
+}
+
+module.exports = SqlUtil;
---
+++ SqlUtilTest.js
@@ -0,0 +1,4 @@
+var SqlUtil = require('SqlUtil');
+
+var sqlUtil = SqlUtil();
+sqlUtil.insert('20180505120000', 'abc', 'def', '', 0);
