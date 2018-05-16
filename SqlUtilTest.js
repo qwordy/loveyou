1b636939c67f513e5d7f25ebf73cb95382b7e0a1
yufeiyue <yufeiyue@baidu.com>
2018-05-16 23:00:03 +0800

    fy

--- SqlUtilTest.js
+++ SqlUtilTest.js
@@ -4,4 +4,8 @@ var sqlUtil = new SqlUtil();
 sqlUtil.insert('20180505120000', 'abc', 'def', '', 0, 0);
 sqlUtil.query('20180501000000', '20180530000000', 1, function(rows) {
   console.log(rows);
+  for (var row in rows) {
+    console.log(row['id']);
+    console.log(row['time']);
+  }
 });
