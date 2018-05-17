77c417298bb31d5261e7d336b39ecaf15669f2d7
yufeiyue <yufeiyue@baidu.com>
2018-05-17 19:54:26 +0800

    fy

--- Bot.js
+++ Bot.js
@@ -23,7 +23,7 @@ class Bot extends BaseBot{
          * s1 is a status variable
          * State:
          * 0: wait new intent
-         * 1: wait for comfirm
+         * 1: wait for comfirmation
          * 2: wait for event
          * 3: wait for time
          * 
@@ -31,14 +31,14 @@ class Bot extends BaseBot{
          * 0 -> 1, when we find time and event, ask confirmation
          * 0 -> 2, when time or event not found, ask event
          * 2 -> 3, always, ask time
-         * 3 -> 1, always
+         * 3 -> 1, always, ask confirmation
          * 1 -> 0, when answer is yes or no
-         * 1 -> 1, when cannot deciding answer
+         * 1 -> 1, when cannot deciding answer, ask confirmation again
          */
         this.addIntentHandler('ai.dueros.common.default_intent', ()=>{
             let s1 = this.getSessionAttribute('s1', 0)
             console.log('s1: ' + s1);
-            if (s1 == 0) {
+            if (s1 == 0) {  // wait new intent
                 if (this.matchRecord(text)) {
                     let timeResult = this.matchTime(text);
                     let pos = timeResult[0];
@@ -58,20 +58,26 @@ class Bot extends BaseBot{
                         return this.makeTextCard('好的，您是要记录' + time + event + '吗？');
                     }
                 }
-            } else if (s1 == 1) {
-                if (this.matchYes(text)) {
+            } else if (s1 == 1) {   // wait for comfirm
+                if (this.matchYes(text)) {  // answer is yes
                     let formatTime = this.getSessionAttribute('formatTime');
                     let event = this.getSessionAttribute('event');
-                } else if (this.matchNo(test)) {
+                    // todo: write db
+                    this.endSession();
+                    return this.makeTextCard('好的，已记录')
+                } else if (this.matchNo(test)) {    // answer is no
                     this.endSession();
                     return this.makeTextCard('好的')
+                } else {    // answer not clear
+
+
                 }
-            } else if (s1 == 2) {
+            } else if (s1 == 2) {   // wait for event
                 let event = text;
                 this.setSessionAttribute('event', event);
                 this.waitAnswer();
                 return this.makeTextCard('请问时间是？');
-            } else if (s1 == 3) {
+            } else if (s1 == 3) {   // wait for time
                 let time = text;
                 let formatTime = this.formatTime(text);
                 this.setSessionAttribute('time', time);
