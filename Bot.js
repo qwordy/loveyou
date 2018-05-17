fcf773e862f9b042441810a0093ed7778e3d115c
yufeiyue <yufeiyue@baidu.com>
2018-05-17 16:27:19 +0800

    fy

--- Bot.js
+++ Bot.js
@@ -22,18 +22,18 @@ class Bot extends BaseBot{
         /**
          * s1 is a status variable
          * State:
-         * 0: waiting new intent
-         * 1: ask for comfirm
-         * 2: ask for event
-         * 3: ask for time
+         * 0: wait new intent
+         * 1: wait for comfirm
+         * 2: wait for event
+         * 3: wait for time
          * 
          * Transform:
-         * 0 -> 1, when we find time and event
-         * 0 -> 2, when time or event not found 
-         * 2 -> 3, always
+         * 0 -> 1, when we find time and event, ask confirmation
+         * 0 -> 2, when time or event not found, ask event
+         * 2 -> 3, always, ask time
          * 3 -> 1, always
          * 1 -> 0, when answer is yes or no
-         * 1 -> 1, when cannot decide whether answer is yes or no
+         * 1 -> 1, when cannot deciding answer
          */
         this.addIntentHandler('ai.dueros.common.default_intent', ()=>{
             let s1 = this.getSessionAttribute('s1', 0)
@@ -44,25 +44,41 @@ class Bot extends BaseBot{
                     let pos = timeResult[0];
                     let time = timeResult[1];
                     let event = text.substr(pos + time.length);
-                    if (time == '' || event == '') {
+                    if (time == '' || event == '') {    // incomplete info
                         this.setSessionAttribute('s1', 2);
+                        this.waitAnswer();
                         return this.makeTextCard('请问您要记录的事件是？');
                     } else {
                         let formatedTime = this.formatTime(time);
                         this.setSessionAttribute('s1', 1);
                         this.setSessionAttribute('time', time);
-                        this.setSessionAttribute('formatTime', time);
+                        this.setSessionAttribute('formatTime', formatedTime);
                         this.setSessionAttribute('event', event);
-                        return this.makeTextCard('您是要记录' + time + event + '吗？');
+                        this.waitAnswer();
+                        return this.makeTextCard('好的，您是要记录' + time + event + '吗？');
                     }
-
                 }
             } else if (s1 == 1) {
                 if (this.matchYes(text)) {
-
+                    let formatTime = this.getSessionAttribute('formatTime');
+                    let event = this.getSessionAttribute('event');
                 } else if (this.matchNo(test)) {
-
+                    this.endSession();
+                    return this.makeTextCard('好的')
                 }
+            } else if (s1 == 2) {
+                let event = text;
+                this.setSessionAttribute('event', event);
+                this.waitAnswer();
+                return this.makeTextCard('请问时间是？');
+            } else if (s1 == 3) {
+                let time = text;
+                let formatTime = this.formatTime(text);
+                this.setSessionAttribute('time', time);
+                this.setSessionAttribute('formatTime', formatedTime);
+                let event = this.getSessionAttribute('event');
+                this.waitAnswer();
+                return this.makeTextCard('好的，您是要记录' + time + event + '吗？');
             }
 
             this.setSessionAttribute('s1', s1 + 1);
@@ -181,7 +197,33 @@ class Bot extends BaseBot{
         }
     }
 
-    matchAsk(text) {}
+    matchYes(text) {
+        let dict = [
+             '是',
+             '是的',
+             '好',
+             '好的',
+             '行',
+             '可以',
+             '没问题',
+             '确认',
+             '确定',
+             '对的',
+             '对',
+             '不错',
+             '嗯'
+        ];
+    }
+
+    matchNo(text) {
+        let dict = [
+            '不是',
+            '否',
+            '不是的',
+            '不',
+            '不对',
+        ];
+    }
 
     /**
      * @param {String} time 
