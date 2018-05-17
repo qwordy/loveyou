f36afe4ff6d4577c665e3bc92ca65fc6bb87aece
yufeiyue <yufeiyue@baidu.com>
2018-05-17 11:49:36 +0800

    fy

--- Bot.js
+++ Bot.js
@@ -8,7 +8,10 @@ class Bot extends BaseBot{
         this.sqlUtil = new SqlUtil();
 
         this.addIntentHandler('LaunchRequest', ()=>{
-            let card = new Bot.Card.TextCard('abc');
+            let card = new Bot.Card.ImageCard();
+            card.addItem('https://upload.wikimedia.org/wikipedia/commons/3/33/-LOVE-love-36983825-1680-1050.jpg');
+            this.setSessionAttribute('s1', 0);
+
             return {
                 card: card,
                 outputSpeech: '欢迎使用有爱'
@@ -16,6 +19,9 @@ class Bot extends BaseBot{
         });
 
         this.addIntentHandler('ai.dueros.common.default_intent', ()=>{
+            s1 = this.getSessionAttribute('s1')
+            console.log(s1);
+            this.setSessionAttribute('s1', s1 + 1);
             //console.log(postData);
             let text = postData['request']['query']['original']
             //let card = new Bot.Card.TextCard(text);
