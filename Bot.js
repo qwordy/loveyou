314cd05b5d84c5a3658a67c2ce370991d4e132a1
yufeiyue <yufeiyue@baidu.com>
2018-05-17 11:52:36 +0800

    fy

--- Bot.js
+++ Bot.js
@@ -11,7 +11,7 @@ class Bot extends BaseBot{
             let card = new Bot.Card.ImageCard();
             card.addItem('https://upload.wikimedia.org/wikipedia/commons/3/33/-LOVE-love-36983825-1680-1050.jpg');
             this.setSessionAttribute('s1', 0);
-
+            this.waitAnswer();
             return {
                 card: card,
                 outputSpeech: '欢迎使用有爱'
@@ -19,9 +19,10 @@ class Bot extends BaseBot{
         });
 
         this.addIntentHandler('ai.dueros.common.default_intent', ()=>{
-            let s1 = this.getSessionAttribute('s1')
+            let s1 = this.getSessionAttribute('s1', 0)
             console.log(s1);
             this.setSessionAttribute('s1', s1 + 1);
+            this.waitAnswer();
             //console.log(postData);
             let text = postData['request']['query']['original']
             //let card = new Bot.Card.TextCard(text);
