ac3ab8df549b75ed65abbcd6f70e79faa3563dd4
yufeiyue <yufeiyue@baidu.com>
2018-05-17 11:32:43 +0800

    fy

--- Bot.js
+++ Bot.js
@@ -8,8 +8,7 @@ class Bot extends BaseBot{
         this.sqlUtil = new SqlUtil();
 
         this.addIntentHandler('LaunchRequest', ()=>{
-            let card = new Bot.Card.ImageCard();
-            card.addItem('https://upload.wikimedia.org/wikipedia/commons/3/33/-LOVE-love-36983825-1680-1050.jpg');
+            let card = new Bot.Card.TextCard('abc');
             return {
                 card: card,
                 outputSpeech: '欢迎使用有爱'
