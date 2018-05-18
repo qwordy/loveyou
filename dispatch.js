d1c3cc186a340ed832f8a192fec81bcbe5d13e3a
Victor Young <13272436992@163.com>
2018-05-18 15:15:07 +0800

    解析意图

--- dispatch.js
+++ dispatch.js
@@ -6,8 +6,14 @@ handlers.set('LaunchRequest', require('./modules/launchRequestHandler'));
 // ... add handlers
 
 // 意图处理分发函数
-function dispatcher() {
+function dispatcher(input) {
     
+}
+
+// 分析意图，返回
+function parseIntent() {
+    // TODO
+    return intentName;
 }
 
 //module.exports.handlers = handlers;
