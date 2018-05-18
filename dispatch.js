1c4400b76cb1bfea3981c250373b8fbe0ae71b98
Victor Young <13272436992@163.com>
2018-05-18 14:48:11 +0800

    分发结构

--- dispatch.js
+++ dispatch.js
@@ -1,1 +1,9 @@
 // 负责主函数与各个子技能之间的分发
+
+var handlers = new Map();
+
+let launch = require('./modules/launchRequestHandler')
+handlers.set('LaunchRequest', launch);
+
+
+module.exports.handlers = handlers;
