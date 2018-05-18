79f494f3ff3ecf14213d49409337d96847cc8071
Victor Young <13272436992@163.com>
2018-05-18 16:29:43 +0800

    qa意图处理

---
+++ modules/qaAnswearRequestHandler.js
@@ -0,0 +1,18 @@
+// Answear and Question
+
+/**
+ * 1. 小度求助
+ * 2. 说出问题xxx，或查看上个问题收到的回复
+ * 3. 3min等待，否则给出默认解答/百度搜索解答
+ * 4. 给赞打分
+ */
+function handleQaAnswearRequest () {
+    console.log('QaAnswearRequestHandler');
+
+}
+
+function defaultAnswear (question) {
+    // 计算问题与答案之书中的答案的相似度，返回最相似答案
+}
+
+module.exports = handleAskingRequest;
