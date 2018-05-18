9d4e8bdbe1a05c9e11bce3ebe724ae0358626e78
Zhao <zhaozhiyong01@baidu.com>
2018-05-18 12:47:33 +0800

    common modules definition

---
+++ config/common.js
@@ -0,0 +1,13 @@
+/**
+公共词库
+公共函数
+*/
+
+class Common {
+
+
+
+}
+
+
+module.exports = Common;
---
+++ models/State.js
@@ -0,0 +1,24 @@
+/**
+唯一标识意图
+1xx 录入重要时间     e.g.记住520是我们在一起的日子
+2xx 预约提醒        e.g. 预约明天买花
+3xx 记录事件        e.g. 告诉今天去了泰山，感觉很好玩
+4xx 询问重要事件    e.g. 最近有什么重要日子？
+5xx 询问去年今天    e.g. 去年我们做了什么？
+
+
+**/
+require('hashmap');
+class State {
+  //意图1
+  this.S1_INIT = 101;
+  this.S1_GET_TIME_OR_EVENT = 102;
+  this.S1_ASK_TIME = 123;
+  this.S1_ASK_CONFIRM = 110;
+  this.S1_ASK_RECONFIRM = 111;
+
+  //意图2
+  this.S2_INIT = 201;
+
+}
+module.exports = State;
