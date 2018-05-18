29217048db1500ea8b02f73dd2153cb0a5b8de44
Zhao <zhaozhiyong01@baidu.com>
2018-05-19 02:39:04 +0800

    add ask event request handler

--- Bot.js
+++ Bot.js
@@ -82,13 +82,6 @@ class Bot extends BaseBot{
           });
     }
 
-    makeTextCard(text) {
-        return {
-            card: new Bot.Card.TextCard(text),
-            outputSpeech: text
-        }
-    }
-
     matchYes(text) {
         let dict = [
              '是',
@@ -240,37 +233,6 @@ class Bot extends BaseBot{
       return this.relativeDaysMap().get(dayText);
     }
 
-//add number of date gap, get Date object
-    addDate(interval,number,date){
-      switch(interval.toLowerCase()){
-      case "y": return new Date(date.setFullYear(date.getFullYear()+number));
-      case "m": return new Date(date.setMonth(date.getMonth()+number));
-      case "d": return new Date(date.setDate(date.getDate()+number));
-      case "w": return new Date(date.setDate(date.getDate()+7*number));
-      case "h": return new Date(date.setHours(date.getHours()+number));
-      case "n": return new Date(date.setMinutes(date.getMinutes()+number));
-      case "s": return new Date(date.setSeconds(date.getSeconds()+number));
-      case "l": return new Date(date.setMilliseconds(date.getMilliseconds()+number));
-      }
-    }
-    addDateFromNow(interval, number) {
-      return this.addDate(interval, number, new Date());
-    }
-
-    //calculate difference between 2 date
-    diffDate(interval,date1,date2){
-        var long = date2.getTime() - date1.getTime(); //相差毫秒
-        switch(interval.toLowerCase()){
-          case "y": return parseInt(date2.getFullYear() - date1.getFullYear());
-          case "m": return parseInt((date2.getFullYear() - date1.getFullYear())*12 + (date2.getMonth()-date1.getMonth()));
-          case "d": return parseInt(long/1000/60/60/24);
-          case "w": return parseInt(long/1000/60/60/24/7);
-          case "h": return parseInt(long/1000/60/60);
-          case "n": return parseInt(long/1000/60);
-          case "s": return parseInt(long/1000);
-          case "l": return parseInt(long);
-        }
-    }
 }
 
 module.exports = Bot;
