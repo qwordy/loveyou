bb2bfbd04cecaf47adf3791eed7ce7578e64080f
Victor Young <13272436992@163.com>
2018-05-18 15:02:31 +0800

    分发

--- dispatch.js
+++ dispatch.js
@@ -1,9 +1,14 @@
 // 负责主函数与各个子技能之间的分发
 
+// 加载所有的意图处理模块
 var handlers = new Map();
+handlers.set('LaunchRequest', require('./modules/launchRequestHandler'));
+// ... add handlers
 
-let launch = require('./modules/launchRequestHandler')
-handlers.set('LaunchRequest', launch);
+// 意图处理分发函数
+function dispatcher() {
+    
+}
 
-
-module.exports.handlers = handlers;
+//module.exports.handlers = handlers;
+module.exports.dispatcher = dispatcher;
