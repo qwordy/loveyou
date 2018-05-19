24ec56e75d95f9206790d61b534bce5ce4b8c292
yufeiyue <yufeiyue@baidu.com>
2018-05-19 09:50:19 +0800

    fy

--- modules/reserveRequestHandler.js
+++ modules/reserveRequestHandler.js
@@ -15,60 +15,63 @@
  * 1 -> 1, when cannot deciding answer, ask confirmation again
  */
 var common = require('../config/common');
+var SqlUtil = require('../SqlUtil');
 module.exports = (bot, input) => {
   let s1 = bot.getSessionAttribute('s1', 0);
   console.log('s1: ' + s1);
   if (s1 == 0) {  // wait new intent
-      if (this.matchRecord(text)) {
-          let timeResult = this.matchTime(text);
+      if (bot.matchRecord(text)) {
+          let timeResult = bot.matchTime(text);
           let pos = timeResult[0];
           let time = timeResult[1];
           let event = text.substr(pos + time.length);
           if (time == '' || event == '') {    // incomplete info
-              this.setSessionAttribute('s1', 2);
-              this.waitAnswer();
-              return this.makeTextCard('请问您要记录的事件是？');
+              bot.setSessionAttribute('s1', 2);
+              bot.waitAnswer();
+              return bot.makeTextCard('请问您要记录的事件是？');
           } else {    // enough info
-              let formatedTime = this.formatTime(time);
-              this.setSessionAttribute('s1', 1);
-              this.setSessionAttribute('time', time);
-              this.setSessionAttribute('formatTime', formatedTime);
-              this.setSessionAttribute('event', event);
-              this.waitAnswer();
-              return this.makeTextCard('好的，您是要记录' + time + event + '吗？');
+              let formatedTime = bot.formatTime(time);
+              bot.setSessionAttribute('s1', 1);
+              bot.setSessionAttribute('time', time);
+              bot.setSessionAttribute('formatTime', formatedTime);
+              bot.setSessionAttribute('event', event);
+              bot.waitAnswer();
+              return bot.makeTextCard('好的，您是要记录' + time + event + '吗？');
           }
       }
   } else if (s1 == 1) {   // wait for comfirm
-      if (this.matchYes(text)) {  // answer is yes
-          let formatTime = this.getSessionAttribute('formatTime');
-          let event = this.getSessionAttribute('event');
-          // todo: write db
-          this.endSession();
-          return this.makeTextCard('好的，已记录');
-      } else if (this.matchNo(text)) {    // answer is no
-          this.endSession();
-          return this.makeTextCard('好的，不记录了');
+      if (bot.matchYes(text)) {  // answer is yes
+          let formatTime = bot.getSessionAttribute('formatTime');
+          let event = bot.getSessionAttribute('event');
+          let sqlUtil = new SqlUtil();
+          // write db
+          sqlUtil.insert(formatTime, '', event, '', 1, '');
+          bot.endSession();
+          return bot.makeTextCard('好的，已记录');
+      } else if (bot.matchNo(text)) {    // answer is no
+          bot.endSession();
+          return bot.makeTextCard('好的，不记录了');
       } else {    // answer not clear
-          let time = this.getSessionAttribute('time');
-          let formatTime = this.getSessionAttribute('formatTime');
-          let event = this.getSessionAttribute('event');
-          this.waitAnswer();
-          return this.makeTextCard('什么，您是要记录' + time + event + '吗？');
+          let time = bot.getSessionAttribute('time');
+          let formatTime = bot.getSessionAttribute('formatTime');
+          let event = bot.getSessionAttribute('event');
+          bot.waitAnswer();
+          return bot.makeTextCard('什么，您是要记录' + time + event + '吗？');
       }
   } else if (s1 == 2) {   // wait for event
       let event = text;
-      this.setSessionAttribute('event', event);
-      this.setSessionAttribute('s1', 3);
-      this.waitAnswer();
-      return this.makeTextCard('请问时间是？');
+      bot.setSessionAttribute('event', event);
+      bot.setSessionAttribute('s1', 3);
+      bot.waitAnswer();
+      return bot.makeTextCard('请问时间是？');
   } else if (s1 == 3) {   // wait for time
       let time = text;
-      let formatTime = this.formatTime(text);
-      this.setSessionAttribute('time', time);
-      this.setSessionAttribute('formatTime', formatTime);
-      let event = this.getSessionAttribute('event');
-      this.setSessionAttribute('s1', 1);
-      this.waitAnswer();
-      return this.makeTextCard('好的，您是要记录' + time + event + '吗？');
+      let formatTime = bot.formatTime(text);
+      bot.setSessionAttribute('time', time);
+      bot.setSessionAttribute('formatTime', formatTime);
+      let event = bot.getSessionAttribute('event');
+      bot.setSessionAttribute('s1', 1);
+      bot.waitAnswer();
+      return bot.makeTextCard('好的，您是要记录' + time + event + '吗？');
   }
 };
